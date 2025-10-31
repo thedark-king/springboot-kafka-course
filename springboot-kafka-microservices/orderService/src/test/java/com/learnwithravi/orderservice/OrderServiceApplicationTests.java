906976a116dfcd88581824768e309408f8ce75bf
Ravi <thedevtool@gmail.com>
2025-10-31 13:46:10 +0530

    Adding order and email services

---
+++ springboot-kafka-microservices/orderService/src/test/java/com/learnwithravi/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnwithravi.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
